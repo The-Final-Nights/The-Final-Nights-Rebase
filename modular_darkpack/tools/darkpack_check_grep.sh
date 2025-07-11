13ca98f0042d7c564143f6cc7ba734a7e632004c
XeonMations <myvilnius@gmail.com>
2025-07-11 11:56:41 +0300

    Update darkpack_check_grep.sh

--- modular_darkpack/tools/darkpack_check_grep.sh
+++ modular_darkpack/tools/darkpack_check_grep.sh
@@ -8,5 +8,5 @@ NC="\033[0m" # No Color
 
 echo -e "${BLUE}Re-running grep checks, but looking in modular_darkpack...${NC}"
 
-# Run the linters again, but modular The Final Nights code.
+# Run the linters again, but modular DarkPack13 code.
 sed "s|code/\*\*/\*\.dm|modular_darkpack/\*\*/\*\.dm|g" <tools/ci/check_grep.sh | bash
