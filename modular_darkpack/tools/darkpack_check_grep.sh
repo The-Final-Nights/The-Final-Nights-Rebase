6c3f38c9e3d4f22cc83f5ce96adc71d152c4f41d
XeonMations <myvilnius@gmail.com>
2025-07-11 11:50:15 +0300

    Update darkpack_check_grep.sh

--- modular_darkpack/tools/darkpack_check_grep.sh
+++ modular_darkpack/tools/darkpack_check_grep.sh
@@ -6,7 +6,7 @@ GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 NC="\033[0m" # No Color
 
-echo -e "${BLUE}Re-running grep checks, but looking in modular_nova...${NC}"
+echo -e "${BLUE}Re-running grep checks, but looking in modular_darkpack...${NC}"
 
 # Run the linters again, but modular The Final Nights code.
 sed "s|code/\*\*/\*\.dm|modular_darkpack/\*\*/\*\.dm|g" <tools/ci/check_grep.sh | bash
