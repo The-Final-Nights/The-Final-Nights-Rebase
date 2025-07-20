ce3861b510b2a3b9cb6912d45fe3d841750ee82c
FalloutFalcon <falloutfalconplays@gmail.com>
2025-07-20 12:20:54 -0500

    change some tg links to darkpack/second city

--- tgui/packages/tgui/interfaces/Changelog.jsx
+++ tgui/packages/tgui/interfaces/Changelog.jsx
@@ -184,26 +184,26 @@ export class Changelog extends Component {
 
     const header = (
       <Section>
-        <h1>Traditional Games Space Station 13</h1>
+        <h1>Darkpack: Second City</h1>
         <p>
           <b>Thanks to: </b>
-          Baystation 12, /vg/station, NTstation, CDK Station devs,
+          The Final Nights, World of Darkness 13, RequiemSS13, TGstation, Baystation 12, /vg/station, NTstation, CDK Station devs,
           FacepunchStation, GoonStation devs, the original Space Station 13
           developers, Invisty for the title image and the countless others who
           have contributed to the game, issue tracker or wiki over the years.
         </p>
         <p>
           {'Current organization members can be found '}
-          <a href="https://github.com/orgs/tgstation/people">here</a>
+          <a href="https://github.com/orgs/DarkPack13/people">here</a>
           {', recent GitHub contributors can be found '}
-          <a href="https://github.com/tgstation/tgstation/pulse/monthly">
+          <a href="https://github.com/DarkPack13/SecondCity/pulse/monthly">
             here
           </a>
           .
         </p>
         <p>
           {'You can also join our discord '}
-          <a href="https://tgstation13.org/phpBB/viewforum.php?f=60">here</a>.
+          <a href="https://discord.gg/rmAbJcuChD">here</a>.
         </p>
         {dateDropdown}
       </Section>
