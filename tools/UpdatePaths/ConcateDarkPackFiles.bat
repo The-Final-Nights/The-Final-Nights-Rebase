0d826cddac141b850b315541dd94630b2025cbfb
FalloutFalcon <falloutfalconplays@gmail.com>
2025-10-04 12:20:09 -0500

    yea

--- tools/UpdatePaths/ConcateDarkPackFiles.bat
+++ tools/UpdatePaths/ConcateDarkPackFiles.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Run this one before you run the normal one so our update paths run first
-type nul > merged.txt
-FOR /R .\Scripts\DarkPack %%f IN (*) DO (type "%%f" >> merged.txt) && (echo. >> merged.txt)
+type nul > darkpackmerged.txt
+FOR /R .\Scripts\DarkPack %%f IN (*) DO (type "%%f" >> darkpackmerged.txt) && (echo. >> darkpackmerged.txt)
