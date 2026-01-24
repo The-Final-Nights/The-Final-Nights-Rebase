b483e7a718d01afa3eb0ddc5f4dbd455e3f5d30f
XeonMations <62395746+XeonMations@users.noreply.github.com>
2026-01-24 20:12:09 +0200

    Update Canvas.tsx

--- tgui/packages/tgui/interfaces/Canvas.tsx
+++ tgui/packages/tgui/interfaces/Canvas.tsx
@@ -449,7 +449,7 @@ export const Canvas = (props) => {
                 <Box bold>
                   {data.author}
                   {!!data.date &&
-                    `- ${new Date(data.date).getFullYear() + 540}`}
+                    `- ${new Date(data.date).getFullYear() - 10}`}
                 </Box>
                 <Box italic>{data.medium}</Box>
                 <Box italic>
